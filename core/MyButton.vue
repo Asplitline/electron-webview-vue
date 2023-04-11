ae389f2cd59ad85ecd064826ff27f5fa8fa4a26e
Asplitline <893666612@qq.com>
2023-04-11 23:22:26 +0800

    chore: init project

---
+++ core/MyButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button>1111</button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ example/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <my-button></my-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MyButton from '@lib/core'
+</script>
+
+<style></style>
